e668b8a4ced83cd8a3856db28c48de717c183eaa
Wasim Almadfaa <wm.madfaa@gmail.com>
2020-06-16 18:17:52 +0200

    fix: task component (accepts long text)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
   stories: ["../src/components/**/*.stories.js"],
-  addons: ["@storybook/preset-create-react-app", "@storybook/addon-actions", "@storybook/addon-links"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-knobs",
+    "@storybook/addon-links",
+  ],
 };
--- src/components/Task/Task.stories.js
+++ src/components/Task/Task.stories.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs/react";
 
 import Task from "./Task";
 
 export default {
   component: Task,
   title: "Task",
+  decorators: [withKnobs],
   excludeStories: /.*Data$/,
 };
 
@@ -20,7 +22,11 @@ export const ActionsData = {
   onPinTask: action("onPinTask"),
   onArchiveTask: action("onArchiveTask"),
 };
+export const longTaskTitleString = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
 
-export const Default = () => <Task task={{ ...TaskData }} {...ActionsData}></Task>;
-export const Pined = () => <Task task={{ ...TaskData, state: "TASK_PINNED" }} {...ActionsData}></Task>;
-export const Archived = () => <Task task={{ ...TaskData, state: "TASK_ARCHIVED" }} {...ActionsData}></Task>;
+export const Default = () => <Task task={object("task", { ...TaskData })} {...ActionsData}></Task>;
+export const Pined = () => <Task task={object("task", { ...TaskData, state: "TASK_PINNED" })} {...ActionsData}></Task>;
+export const Archived = () => (
+  <Task task={object("task", { ...TaskData, state: "TASK_ARCHIVED" })} {...ActionsData}></Task>
+);
+export const LongTitle = () => <Task task={{ ...TaskData, title: longTaskTitleString }} {...ActionsData} />;
